16b1ee0ed53420ba9c1ec45e1d2d19d3e60adb6e
JavatoX <cdpm98@hotmail.com>
2023-06-19 12:01:26 -0600

    MODIFICACIÓN DE ESTILOS Y FUNCIONES

--- wp-content/themes/gymfitness/index.php
+++ wp-content/themes/gymfitness/index.php
@@ -5,11 +5,27 @@
   <title></title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="css/style.css" rel="stylesheet">
+  <title>GymFitness</title>
+  <?php wp_head(); ?>
 </head>
 
 <body>
-  <header>
+  <header class="header">
+    <div class="contenedor barra-navegacion">
+      <div class="logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logotipo">
+      </div>
+
+      <?php
+      $args = array(
+        'theme_location' => 'menu-principal',
+        'container' => 'nav',
+        'container_class' => 'menu-principal'
+      );
+
+      wp_nav_menu($args);
+      ?>
+    </div>
 
   </header>
 
